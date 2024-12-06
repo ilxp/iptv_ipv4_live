554fd3bd09f43623705817a7924a1d132783d17d
ilxp <lixp@live.com>
2024-12-06 14:01:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# 格式区别
+TXT格式不含台标。
+M3U配置peg和台标!
+
+diyp，影视仓请使用txt格式。mytv，酷9请使用m3u或者txt。其余播放器没有测试，自己试吧。
+
+# M3U格式加速地址
+*ipv4地址：
+* [国内加速1](https://ghp.ci/https://raw.githubusercontent.com/ilxp/iptv/main/ipv4.m3u)
+* [国内加速2](https://ghproxy.cn/https://raw.githubusercontent.com/ilxp/iptv/main/ipv4.m3u)
+
+*ipv6地址：
+* [国内加速1](https://ghp.ci/https://raw.githubusercontent.com/ilxp/iptv/main/ipv6.m3u)
+* [国内加速2](https://ghproxy.cn/https://raw.githubusercontent.com/ilxp/iptv/main/ipv6.m3u)
+
+# TXT格式加速地址
+
+*ipv4地址：
+* [国内加速1](https://ghp.ci/https://raw.githubusercontent.com/ilxp/iptv/main/ipv4.txt)
+* [国内加速2](https://ghproxy.cn/https://raw.githubusercontent.com/ilxp/iptv/main/ipv4.txt)
+
+*ipv6地址：
+* [国内加速1](https://ghp.ci/https://raw.githubusercontent.com/ilxp/iptv/main/ipv6.txt)
+* [国内加速2](https://ghproxy.cn/https://raw.githubusercontent.com/ilxp/iptv/main/ipv6.txt)
---
+++ script_m3u-v4.py
@@ -0,0 +1,35 @@
+import requests
+#url = 'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.m3u'  # 替换为你想下载的文件的URL
+url = 'https://raw.githubusercontent.com/ilxp/YKTV/main/live.m3u'
+local_filename = 'ipv4.m3u'  # 指定保存到本地的文件名
+ 
+with requests.get(url, stream=True) as r:
+    with open(local_filename, 'wb') as f:
+        f.write(r.content)
+ 
+ 
+def remove_lines_with_aaa(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        
+    # 创建一个新的列表用于保存要保留的行
+    new_lines = []
+    skip_next = False  # 标记是否跳过下一行
+
+    for i in range(len(lines)):
+        if skip_next:
+            skip_next = False
+            continue
+        
+        if 'IPV6' in lines[i]:
+            skip_next = True  # 如果当前行包含'aaa'，标记跳过下一行
+            continue
+        
+        new_lines.append(lines[i])  # 保留当前行
+
+    # 将结果写回文件或另存为新文件
+    with open(file_path, 'w') as file:
+        file.writelines(new_lines)
+
+# 使用示例
+remove_lines_with_aaa('ipv4.m3u')
